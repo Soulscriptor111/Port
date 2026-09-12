1d5708c5903c44a41a6bc2ff794f4f71e8404369
Alain_Rw <alaingentilg2011@gmail.com>
2026-09-12 12:16:58 +0200

    Fix FORMSPREE_ENDPOINT string formatting

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -2,7 +2,7 @@
 import { ref } from "vue";
 import socialLinks from "../../data/media.js";
 
-const FORMSPREE_ENDPOINT = https://formspree.io/f/xrpzazep;
+const FORMSPREE_ENDPOINT = "https://formspree.io/f/xrpzazep";
 
 const form = ref({ name: "", email: "", subject: "", message: "" });
 const status = ref("idle");
